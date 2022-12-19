e14da4784b37eb681ea797cdd021fbb08684d18d
Tim Storer <tim.storer@ocado.com>
2022-12-19 15:22:36 +0000

    Redirect

--- redirect/main.js
+++ redirect/main.js
@@ -3,13 +3,18 @@ getInput();
 
 function getInput(text) {
     const input = document.getElementById('input');
+    const button = document.getElementById('button');
     input.addEventListener('change', entered);
-    return '';
+    button.addEventListener('click', entered);
 }
 
 function entered() {
     var input_url=document.getElementById('input').value;
 
     var url=input_url.replace('twitter.com','nitter.nixnet.services');
+    var s=document.createElement('p');
+    s.textContent=url;
+    s.href=url;
+    document.body.appendChild(s);
     window.location.replace(url);
 }
